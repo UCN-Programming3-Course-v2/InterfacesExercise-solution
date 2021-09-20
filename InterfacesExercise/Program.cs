1c376653e9d46331cd8b0e909573357230e9c85f
Lars Nysom <LANY@ucn.dk>
2021-09-20 13:17:21 +0200

    Updated syntax

--- InterfacesExercise/Program.cs
+++ InterfacesExercise/Program.cs
@@ -9,19 +9,20 @@ namespace InterfacesExercise
         static void Main(string[] args)
         {
             // Instantiate House and Boat objects
-            House house1 = new House("abc", 200, 10000, "Good", "A fine house in the country");
-            House house2 = new House("Dabc", 100, 50000, "Bad", "A trashy house in the city");
+            House house1 = new("abc", 200, 10000, "Good", "A fine house in the country");
+            House house2 = new("Dabc", 100, 50000, "Bad", "A trashy house in the city");
 
-            Boat boat1 = new Boat(30, 10000, "Good", "seasick generator", 1990);
-            Boat boat2 = new Boat(10, 1000, "Good", "Bathtub", 2006);
+            Boat boat1 = new(30, 10000, "Good", "seasick generator", 1990);
+            Boat boat2 = new(10, 1000, "Good", "Bathtub", 2006);
 
             // Add objects to an ArrayList
-            ArrayList list1 = new ArrayList();
-
-            list1.Add(house1);
-            list1.Add(house2);
-            list1.Add(boat1);
-            list1.Add(boat2);
+            ArrayList list1 = new()
+            {
+                house1,
+                house2,
+                boat1,
+                boat2
+            };
 
             int price = 0;
             string invSummary = null;
